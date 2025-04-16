1ce05fd7184b05bfd84116378154e6dea9fc6806
lazoprogram <mamuchashvililazare2@gmail.com>
2025-04-16 15:20:17 +0400

    commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,74 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1:  draw a square
+speed(10)
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+begin_fill()
+forward(70)
+color("green")
+left(90)
+forward(120) #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+penup()
+goto(200, 200)
+pendown()
+
+color("brown")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#drawing a  windows
+
+color("yellow")
+left(30)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+
+
+
+right(180)
+forward(200)
+left(180)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+
+
+
+
+
+exitonclick()
