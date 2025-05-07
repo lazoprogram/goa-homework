dba365f6ec81ba854f3c2ab7cb0e3cc026644523
lazoprogram <mamuchashvililazare2@gmail.com>
2025-05-07 21:39:21 +0400

    commit

---
+++ day 02/homework/homework.py
@@ -0,0 +1,26 @@
+number1 = 200
+number2 = 40
+print(number1 * 2)
+print(number2 - 10)
+print(number1 / 4)
+print(number2 + 40)
+
+
+birth_year = 2011
+current_year = 2025
+age = current_year - birth_year
+print(age)
+
+
+number1 = 30
+number2 = 5
+number3 = 2
+number4 = 10
+number5 = 20
+print((number1 + number2 + number3 + number4 + number5) / 5)
+
+#+ ეს გამოიყენება მიმატებისთვის
+#- ეს გამოიყენეა გამოკლებისთვის
+#* ეს გამოიყენება გამრავლებისთვის
+#/ ეს გამოიყენება გაყოფისთვის , ეს ყოველთვის აბრუნებს ათწილადს
+#// ეს გამოიყენება უბრალოდ გაყოფისთვის , ეს ყოველთვის აბრუნებს ინტეჯერს
