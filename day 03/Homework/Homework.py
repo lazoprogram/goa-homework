4c4aa02ec29cc842e3eddb0d79e780fb7fa427bf
lazoprogram <mamuchashvililazare2@gmail.com>
2025-05-12 14:53:50 +0400

    commit

---
+++ day 03/Homework/Homework.py
@@ -0,0 +1,4 @@
+# web development - შეიცავს ორ ნაწილს FrontEnd და BackEnd, FrontEnd შეიცავს Html,javascript,CSS და FrontEnd Structure HTML Style CSS interactivity JavaScript,  BackEnd შეიცავს
+# sever,DataBase,Api,Java,python,C++
+# empty tag - ისეთი ელემენტი, რომელსაც მხოლოდ გამხსნელი თეგი აქვს. მაგალითად <img>
+# container tag - ისეთი ელემნთი, რომელსაც აქვს როგორც გამხსნელი, ასევე დამხურავი თეგი. მაგალითად <p>Lazo</p>
