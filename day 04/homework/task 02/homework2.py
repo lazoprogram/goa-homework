259e7f9052c182a85773b5f16cec77c31c305a25
lazoprogram <mamuchashvililazare2@gmail.com>
2025-05-24 22:06:15 +0400

    commit

---
+++ day 04/homework/task 02/homework2.py
@@ -0,0 +1,34 @@
+country = "iceland"
+age = 14
+print(age , country)
+
+# შექმენით ორი float ტიპის ცვლადი
+length = 5.5  # სიგრძე
+width = 3.2   # სიგანე
+
+# გამოთვალეთ მართკუთხედის ფართობი
+area = length * width
+
+# დაბეჭდეთ შედეგი
+print("მართკუთხედის ფართობია area")
+
+
+# შექმენით str ტიპის ცვლადი ნივთის სახელისთვის და float ტიპის ცვლადი ფასისთვის
+item_name = "ნოუბუქი"  # ნივთის სახელი
+item_price = 1500.75     # ნივთის ფასი
+
+# დაბეჭდეთ წინადადება, რომელიც აერთიანებს ორივეს
+print(f"ნივთი: {item_name}, ფასია: {item_price} ლარი.")
+
+
+age = 14
+age = 14
+
+print(age + age)
+
+item_price = 200 
+item_price1 = 2
+print(item_price / item_price1)
+
+
+
