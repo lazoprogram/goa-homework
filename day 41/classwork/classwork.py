c4bb5b7f65ea5ef743f212e633615b6b901a3459
lazoprogram <mamuchashvililazare2@gmail.com>
2026-02-28 19:16:07 +0400

    commit

--- day 41/classwork/classwork.py
+++ day 41/classwork/classwork.py
@@ -17,3 +17,15 @@ def remove_char(s):
 
 def is_palindrome(s):
         return s.lower() == s[::-1].lower()
+
+
+#https://www.codewars.com/kata/55a996e0e8520afab9000055/train/python
+
+
+def cookie(x):
+    if type(x) == str:
+        return "Who ate the last cookie? It was Zach!"
+    if type(x) == int or type(x) == float:
+        return "Who ate the last cookie? It was Monica!"
+    else:
+        return "Who ate the last cookie? It was the dog!"
